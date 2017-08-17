f93b6e6905a16a0d735b544f13f75382c824228d
celine31770 <celinemercadier@outlook.com>
2017-08-17 21:40:24 +0200

    1 monstre ok left aleatoire

--- alice.js
+++ alice.js
@@ -25,14 +25,22 @@ var bordure_Y = 0;
 var pasAlice;
 let posAlice = parseInt(aliceRecupCSS.left.replace("px", ""));
 let touche = [];
-
+// ## GESTION DU MISSILE
 let missilePresent = false;
-var positionMissile;
+
 var cibleMissile;
 var missileRecupCSS;
+var positionMissileX;
 var positionMissileY;
+// ## GESTION D UN ENNEMI
+let ennemiVivant = false;
 
-
+var ennemi;
+var cibleEnnemi;
+var ennemiRecupCSS;
+var positionEnnemiX;
+var positionEnnemiY;
+var positionEnnemiAleaX;
 
 // ## FONCTION LOOP ################################################
 function loop(){
@@ -45,49 +53,11 @@ function loop(){
 
 deplacement_bg();
 deplacement_bordure();
-//pascal detection touches-> attribution du pas
-    // Gestion des touches du clavier
-    if (touche[37]===true && posAlice > positionMini) {
-        // console.log(posAlice+' '+positionMini);
-        posAlice-=5;
-        alice.style.left = posAlice + 'px';
-    }
-    if (touche[39]===true && posAlice < positionMaxi) {
-        // console.log(posAlice+' '+positionMaxi);
-        posAlice+=5;
-        alice.style.left = posAlice + 'px';
-    }
-
-
-    if ((touche[32]===true) && (!missilePresent)) {
-        console.log('touche 32');
-        missilePresent = true;
-          //## CREATION MISSILE
-        positionMissile = posAlice + demialiceWidth -10;
-        missile = document.createElement('div');
-        missile.setAttribute('class', 'missileTire');
-        missile.style.top = 100 + 'px';
-        missile.style.left = positionMissile + 'px';
-        cible.appendChild(missile);
-    }
-
-
-if(missilePresent){
-    cibleMissile = document.querySelector('.missileTire');
-    missileRecupCSS = getComputedStyle(cibleMissile);
-    positionMissileY = parseInt(missileRecupCSS.top.replace("px", ""));
-    positionMissileY += 2;
-    missile.style.top = positionMissileY + 'px';
-
-    if(positionMissileY >= 600){
-        cible.removeChild(missile);
-        missilePresent = false;
-        // console.log("atteint 400 px");
-    }
-}
-
-
 gestionClavier();
+deplacementAlice();
+missileF();
+ennemiF();
+
 //# REFRESH
 requestAnimationFrame(loop);
 }
@@ -127,8 +97,87 @@ function gestionClavier() {
     touche[event.which] = false;
   });
 }
+function deplacementAlice(){
+    //pascal detection touches-> attribution du pas
+        // Gestion des touches du clavier
+        if (touche[37]===true && posAlice > positionMini) {
+            // console.log(posAlice+' '+positionMini);
+            posAlice-=5;
+            alice.style.left = posAlice + 'px';
+        }
+        if (touche[39]===true && posAlice < positionMaxi) {
+            // console.log(posAlice+' '+positionMaxi);
+            posAlice+=5;
+            alice.style.left = posAlice + 'px';
+        }
+}
+function missileF(){
+    if ((touche[32]===true) && (!missilePresent)) {
+        // console.log('touche 32');
+        missilePresent = true;
+          //## CREATION MISSILE
+        positionMissileX = posAlice + demialiceWidth -10;
+        missile = document.createElement('div');
+        missile.setAttribute('class', 'missileTire');
+        missile.style.top = 100 + 'px';
+        missile.style.left = positionMissileX + 'px';
+        cible.appendChild(missile);
+    }
+    //deplacement du missile
+    if(missilePresent){
+        cibleMissile = document.querySelector('.missileTire');
+        missileRecupCSS = getComputedStyle(cibleMissile);
+        positionMissileY = parseInt(missileRecupCSS.top.replace("px", ""));
+        positionMissileY += 2;
+        missile.style.top = positionMissileY + 'px';
+        //destruction mmissile en bas
+        if(positionMissileY >= 600){
+            cible.removeChild(missile);
+            missilePresent = false;
+            // console.log("atteint 400 px");
+        }
+    }
+}
+function ennemiF(){
+    if(!ennemiVivant){
+        //fabrique un ennemi, passe vivant a true
+        //console.log('touche 139');
+        ennemiVivant = true;
+        ennemi = document.createElement('div');
+        ennemi.setAttribute('class', 'ennemiFabrique');
+        ennemi.style.top = 500 + 'px' ;
+        //la position en Y aleatoire, 100 correspond a la largeur de la div
+        //plus tard, on affectera une var monstre1; var monstre2; chaque monstre aura une width definie
+        //en fnction du monstre genere, la largeur sera connue; propriete des monstres ->tab??
+        positionEnnemiAleaX = Math.floor(Math.random() * (800-100));
+        ennemi.style.left = positionEnnemiAleaX + 'px' ;
+        cible.appendChild(ennemi);
+        console.log("ligne 144");
+    }
+    //deplacement
+    if(ennemiVivant){
+        cibleEnnemi= document.querySelector('.ennemiFabrique');
+        ennemiRecupCSS = getComputedStyle(cibleEnnemi);
+        positionEnnemiY = parseInt(ennemiRecupCSS.top.replace("px", ""));
+        // console.log("ligne 156" + positionEnnemiY);
+        positionEnnemiY -= 3;
+        ennemi.style.top = positionEnnemiY + 'px';
+    }
+    //tue si ennemi arrive en haut
+    if(positionEnnemiY <= -100){
+        cible.removeChild(ennemi);
+        ennemiVivant = false;
+        // console.log("atteint 400 px");
+    }
 
+}
+//let ennemiVivant = false;
 
+// var ennemi;
+// var cibleEnnemi;
+// var ennemiRecupCSS;
+// var positionEnnemiX;
+// var positionEnnemiY;
 
 
 loop();
