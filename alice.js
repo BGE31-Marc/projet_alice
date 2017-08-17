f71f00fcde7cfe5858ae4310de949a297b3726ae
celine31770 <celinemercadier@outlook.com>
2017-08-17 14:59:29 +0200

    missile genere place

--- alice.js
+++ alice.js
@@ -8,6 +8,10 @@ var bordureD = document.querySelector('#bordureD');
 var alice = document.querySelector('#alice');
 var cible = document.querySelector('.jeuxALICE');
 var aliceRecupCSS = getComputedStyle(alice);
+//largeur de alice
+var aliceWidth = parseInt(aliceRecupCSS.width.replace("px", ""));
+//demi alice
+var demialiceWidth = aliceWidth/2;
 // ## taille de le zone de jeux
 var zoneDeJeuxRecupCSS = getComputedStyle(zoneDeJeux);
 //## DEPLACEMENT DU DECORS##
@@ -15,23 +19,17 @@ var bg_Y = 0;
 var bordure_Y = 0;
 //## DEPLACEMENT ALICE ##
 var pasAlice;
-//pascal
-// Initialisation
 let posAlice = aliceRecupCSS.left.replace("px", "");
 let touche = [];
-//## CREATION MISSILE
-var missile = document.createElement('div');
-missile.setAttribute('class', 'missileTire');
-// missile.setAttribute('height', '50px');
-// missile.setAttribute('background-color', 'red');
+
+
 
 // ## FONCTION LOOP ################################################
 function loop(){
+
     //## POSITION DE LA ZONE DE JEUX A CHAQUE MOMENT ->REACTUALISEE
     var positionMini = parseInt(zoneDeJeuxRecupCSS.left.replace("px", ""));
     var tailleZoneDeJeux = parseInt(zoneDeJeuxRecupCSS.width.replace("px", ""));
-    //largeur de alice
-    var aliceWidth = parseInt(aliceRecupCSS.width.replace("px", ""));
     var positionMaxi = parseInt(positionMini + tailleZoneDeJeux - aliceWidth);
 
 
@@ -40,21 +38,26 @@ deplacement_bordure();
 //pascal detection touches-> attribution du pas
     // Gestion des touches du clavier
     if (touche[37]===true && posAlice > positionMini) {
-      // alice.style.transform = "scaleX(1)";
       posAlice-=5;
       alice.style.left = posAlice + 'px';
     }
     if (touche[39]===true && posAlice < positionMaxi) {
-      // alice.style.transform = "scaleX(-1)";
       posAlice+=5;
       alice.style.left = posAlice + 'px';
     }
     if (touche[32]===true) {
       console.log('touche 32');
-      cible.appendChild('missile');
+      //## CREATION MISSILE
+      var positionMissile = posAlice + demialiceWidth;
+      var missile = document.createElement('div');
+      missile.setAttribute('class', 'missileTire');
+      missile.style.top = 100 + 'px';
+      missile.style.left = positionMissile + 'px';
+      cible.appendChild(missile);
     }
-deplacementAlice();
-// fabriqueMissile();
+gestionClavier();
+
+
 //# REFRESH
 requestAnimationFrame(loop);
 }
@@ -76,7 +79,6 @@ function deplacement_bg(){
     bg_Y -= 1;
     if(bg_Y >= 600) {bg_Y = 0;}
         zoneDeJeux.style.backgroundPositionY = bg_Y + 'px';
-    // console.log(bg_Y + 'px');
 }
 function deplacement_bordure(){
     bordure_Y -= 2;
@@ -84,7 +86,7 @@ function deplacement_bordure(){
         bordureG.style.backgroundPositionY = bordure_Y + 'px';
         bordureD.style.backgroundPositionY = bordure_Y + 'px';
 }
-function deplacementAlice() {
+function gestionClavier() {
   // Initialisation
   // Détection de touche enfoncée
   document.addEventListener('keydown', function(event) {
@@ -94,21 +96,8 @@ function deplacementAlice() {
   document.addEventListener('keyup', function(event) {
     touche[event.which] = false;
   });
-  // Démarrage de la LOOP
-  // gameLoop();
 }
 
-// function fabriqueMissile(){
-//     // Détection de touche enfoncée
-//     document.addEventListener('keydown', function(event) {
-//           touche[event.which] = true;
-//     });
-//     // détection de touche non enfoncée
-//     document.addEventListener('keyup', function(event) {
-//       touche[event.which] = false;
-//     });
-//     // console.log('touche 32 ');
-// }
 
 
 
